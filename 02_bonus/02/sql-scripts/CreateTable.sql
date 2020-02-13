180161f2c1ea4f29ff50e8fe15887ad6d8ea224f
Cressen Meowth <cmeowth@oa-b5.21-school.ru>
2020-02-13 13:33:26 +0300

    docker

---
+++ 02_bonus/02/sql-scripts/CreateTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE projects (
+	name varchar(25),
+	mark  varchar(25),
+	status varchar(15),
+	XP  varchar(50)
+);
---
+++ 02_bonus/02/sql-scripts/InsertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO projects (name, mark, status, XP) 
+VALUES ('docker-1', '-', 'in progress', '100 XP');
+INSERT INTO projects (name, mark, status, XP)
+VALUES ('roger-skyline-1', '100', 'success', '100 XP');
